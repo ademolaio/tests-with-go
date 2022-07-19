58f68788e8320f405909b65d17b0334d637e2ee7
ademolaio <samuel.ademola@gmail.com>
2022-07-19 07:04:44 -0500

    wrote a subset test.

--- ch_01/hello_test.go
+++ ch_01/hello_test.go
@@ -3,10 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T)  {
-	got := Hello()
-	want := "Hello, world"
+	t.Run("Saying hello to people", func(t *testing.T) {
 
-	if got != want {
-		t.Errorf("Got %q, but want %q", got, want)
-	}
+		got := Hello("Chris")
+		want := "Hello, Chris"
+	
+		if got != want {
+			t.Errorf("Got %q, but want %q", got, want)
+		}
+	})
+
+	t.Run("Say 'Hello, World' when an empty string is supplied", func (t *testing.T)  {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got %q, but want %q", got, want)
+		}
+	})
+
 }
