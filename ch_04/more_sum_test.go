25b96de4512027ef9237f480c5bebb080bc9881a
ademolaio <samuel.ademola@gmail.com>
2022-07-27 06:26:52 -0500

    finished learnig slcies.

--- ch_04/more_sum_test.go
+++ ch_04/more_sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestMoreSum(t *testing.T) {
 	t.Run("Collection of 5 numbers", func(t *testing.T) {
@@ -22,4 +25,29 @@ func TestMoreSum(t *testing.T) {
 			t.Errorf("Got %d want %d given, %v", got, want, numbers)
 		}
 	})
+}
+
+
+
+
+func TestSumAllTails(t *testing.T)  {
+
+	t.Run("Make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v but want %v", got, want)
+		}
+	})
+
+	t.Run("Safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int {0, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v but want %v", got, want)
+		}
+	})
+
 }
